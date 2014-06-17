5c238e7cd344acaf08be9291ce4bd5acf05edd54
Flammenmensch <maleventum@gmail.com>
2014-06-18 00:59:26 +0400

    Completed application

--- EightBall/ViewController.m
+++ EightBall/ViewController.m
@@ -9,8 +9,20 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+- (void)fadeFortune;
+- (void)newFortune;
 @end
+
+static NSString *gAnswers[] = {
+    @"\rYES",
+    @"\rNO",
+    @"\rMAYBE",
+    @"I\rDON'T\rKNOW",
+    @"TRY\rAGAIN",
+    @"READ\rTHE\rMANUAL"
+};
+
+#define kNumberOfAnswers (sizeof(gAnswers)/sizeof(NSString*))
 
 @implementation ViewController
 
@@ -24,6 +36,39 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)fadeFortune {
+    [UIView animateWithDuration:0.75 animations:^{
+        self.answerView.alpha = 0.0;
+    }];
+}
+
+- (void)newFortune {
+    self.answerView.text = gAnswers[arc4random_uniform(kNumberOfAnswers)];
+    
+    [UIView animateWithDuration:2.0 animations:^{
+        self.answerView.alpha = 1.0;
+    }];
+}
+
+// Motion methods
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake) {
+        [self fadeFortune];
+    }
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake) {
+        [self newFortune];
+    }
+}
+
+- (void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake) {
+        [self newFortune];
+    }
 }
 
 @end
